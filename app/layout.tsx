bfdc2e5c5f8fbfb5d0976c876feb176c8a09fa69
Tanvir Hasan <72351875+tanvir-eng@users.noreply.github.com>
2025-07-12 15:12:39 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,11 @@ export const metadata = {
   description: 'Check your transaction count on Irys Network Testnet',
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
       <body>{children}</body>
